7d8844d39c26d2c513bfabfe5d5adad49f2eb998
Vaibhav Shukla <vaibhavshukla264@gmail.com>
2025-08-22 19:06:55 +0530

    arrays

---
+++ arrays/power.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// 50. Pow(x, n)
+double  power(double x, int n){
+  if(n==0) return 1;
+  if(x==0) return 0;
+  if(n==1) return x;
+  if(x==1) return 1;
+  
+  double ans = 1;
+  long binary = n;
+
+  if(n<0){
+    x = 1/x;
+    binary = -binary;
+  }
+  while(binary>0){
+    if(binary%2==1){
+      ans*=x;
+    }
+    x*=x;
+    binary/=2;
+  }
+  return ans;
+}
+int main(){
+
+  // double x = 2.00000;
+  // int n = -2;
+
+  double x = 2.00000;
+  int n = -2147483648;
+
+  cout << power(x, n);
+  return 0;
+}
